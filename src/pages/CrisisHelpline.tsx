86d7a8f4f7f52d3e765d77c775dbdf3ae0b0dd10
vaishnavi paturkar <vaishanavipaturkar162@gmail.com>
2026-02-22 19:18:38 +0530

    crisis againn

--- src/pages/CrisisHelpline.tsx
+++ src/pages/CrisisHelpline.tsx
@@ -159,7 +159,7 @@ export default function CrisisHelpline() {
 
   const submitRequest = async () => {
     try {
-      await fetch(`${API_URL}/api/crisis`, {
+      await fetch(`${API_URL}/crisis`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
